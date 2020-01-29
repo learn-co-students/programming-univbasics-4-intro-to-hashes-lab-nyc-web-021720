dd6e3669d42b1bbcfcf128f71f256da1772fe267
Jacky Lin <jacky.jia.lin@gmail.com>
2020-01-29 17:44:36 +0000

    Done.

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -75,3 +75,41 @@ describe "updating data in a hash" do
   end
 
 end
+
+
+def new_hash
+  {}
+end 
+
+def my_hash 
+  { 
+    :message => "Hello World"
+  }
+end 
+
+def pioneer
+  {
+    :name => "Grace Hopper"
+  }
+end 
+
+def id_generator
+  id_generator = {:id => 3}
+end
+  
+def my_hash_creator(key,value)
+  my_hash_creator = {key => value}
+end 
+
+def read_from_hash(hash, key)
+  hash[key]
+end
+
+def update_counting_hash(hash, key)
+  if hash.key?(key)
+    hash[key] += 1 
+  else
+    hash[key] = 1
+  end
+  hash
+end
